b770bacdd244580033c4653cedf154b6af306b30
Daniel Holmberg <danielholmberg.dev@gmail.com>
2019-03-31 22:01:47 +0200

    Initial test project with React + Express

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import PasswordGenerator from './PasswordGenerator/PasswordGenerator';
+
+class App extends Component {
+  
+  render() {
+    return (
+      <PasswordGenerator/>
+    );
+  }
+}
+
+export default App;
