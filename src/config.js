41d32effcc640f54a8abdc6716a9ba3c3ecf17f3
Daniel Holmberg <danielholmberg.dev@gmail.com>
2019-04-20 21:10:07 +0200

    Changed S3 Bucket for attachment uploads due to Region issue

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ export default {
   MAX_ATTACHMENT_SIZE: 5000000,
   s3: {
     REGION: "us-east-2",
-    BUCKET: "bubbler-uploads"
+    BUCKET: "bubbler-user-uploads"
   },
   apiGateway: {
     REGION: "us-east-2",
