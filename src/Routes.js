055294622933cde99c30b755221fb0c2c7d50b8b
Daniel Holmberg <danielholmberg.dev@gmail.com>
2019-04-25 12:58:24 +0200

    Added Code Splitting to speed up initial loadtime

--- src/Routes.js
+++ src/Routes.js
@@ -1,22 +1,67 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import Home from "./containers/Home/Home.js";
-import Login from "./containers/Login/Login.js";
+import Loadable from "react-loadable";
+
+// Components
 import AppliedRoute from './components/AppliedRoute.js';
-import Signup from "./containers/Signup/Signup.js";
-import NotFound404 from "./containers/NotFound404/NotFound404.js";
-import NewWine from "./containers/NewWine/NewWine.js";
-import Wines from "./containers/Wines/Wines.js";
 import AuthenticatedRoute from "./components/AuthenticatedRoute.js";
 import UnauthenticatedRoute from "./components/UnauthenticatedRoute.js";
 
+const MyLoadingComponent = ({error, timedOut, retry, pastDelay}) => {
+  if (error) {
+    return <div>Sorry, there was a problem loading the page.</div>;
+  } else if (timedOut) {
+    return <div>Taking a long time... <button onClick={retry}>Retry</button></div>;
+  } else if (pastDelay) {
+    return <div>Bubbling...</div>;
+  } else {
+    return null;
+  }
+};
+
+// Containers
+const AsyncHome = Loadable({
+  loader: () => import("./containers/Home/Home.js"),
+  loading: MyLoadingComponent,
+  timeout: 10000, // 10 seconds
+  delay: 300, // 0.3 seconds
+});
+const AsyncLogin = Loadable({
+  loader: () => import("./containers/Login/Login.js"),
+  loading: MyLoadingComponent,
+  timeout: 10000, // 10 seconds
+  delay: 300, // 0.3 seconds
+});
+const AsyncSignup = Loadable({
+  loader: () => import("./containers/Signup/Signup.js"),
+  loading: MyLoadingComponent,
+  timeout: 10000, // 10 seconds
+  delay: 300, // 0.3 seconds
+});
+const AsyncNewWine = Loadable({
+  loader: () => import("./containers/NewWine/NewWine.js"),
+  loading: MyLoadingComponent,
+  timeout: 10000, // 10 seconds
+  delay: 300, // 0.3 seconds
+});
+const AsyncWines = Loadable({
+  loader: () => import("./containers/Wines/Wines.js"),
+  loading: MyLoadingComponent,
+  timeout: 10000, // 10 seconds
+  delay: 300, // 0.3 seconds
+});
+const AsyncNotFound404 = Loadable({
+  loader: () => import("./containers/NotFound404/NotFound404.js"),
+  loading: MyLoadingComponent,
+  timeout: 10000, // 10 seconds
+  delay: 300, // 0.3 seconds
+});
 /**
  * This component uses this Switch component from React-Router that 
  * renders the first matching route that is defined within it. 
- * For now we only have a single route, it looks for / and renders 
- * the Home component when matched. We are also using the exact prop 
- * to ensure that it matches the / route exactly. This is because the 
- * path / will also match any route that starts with a /.
+ * The first route looks for / and renders the Home component when matched. 
+ * We are also using the exact prop to ensure that it matches the / route exactly. 
+ * This is because the path / will also match any route that starts with a /.
  * 
  * By using the route path /wines/:id we are telling the router to send all matching 
  * routes to our component Wines. This will also end up matching the route /wines/new 
@@ -25,13 +70,13 @@ import UnauthenticatedRoute from "./components/UnauthenticatedRoute.js";
  */
 export default ({ childProps }) =>
   <Switch>
-    <AppliedRoute path="/" exact component={Home} props={childProps} />
-    <UnauthenticatedRoute path="/login" exact component={Login} props={childProps} />
-    <UnauthenticatedRoute path="/signup" exact component={Signup} props={childProps} />
-    <AuthenticatedRoute path="/wines/new" exact component={NewWine} props={childProps} />
-    <AuthenticatedRoute path="/wines/:id" exact component={Wines} props={childProps} />
+    <AppliedRoute path="/" exact component={AsyncHome} props={childProps} />
+    <UnauthenticatedRoute path="/login" exact component={AsyncLogin} props={childProps} />
+    <UnauthenticatedRoute path="/signup" exact component={AsyncSignup} props={childProps} />
+    <AuthenticatedRoute path="/wines/new" exact component={AsyncNewWine} props={childProps} />
+    <AuthenticatedRoute path="/wines/:id" exact component={AsyncWines} props={childProps} />
 
 
     { /* Finally, catch all unmatched routes */ }
-    <Route component={NotFound404} />
+    <Route component={AsyncNotFound404} />
   </Switch>;
