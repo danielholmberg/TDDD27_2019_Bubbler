0e379b7ad9e2ad0a3f0943fd631a55999409d0d6
Daniel Holmberg <danielholmberg.dev@gmail.com>
2019-04-28 14:43:16 +0200

    Added API endpoint /systembolaget to retrieve all sparkling wines from Systembolaget

---
+++ getSystembolagetData.js
@@ -0,0 +1,16 @@
+import { success, failure } from "./libs/response-lib";
+import SystembolagetData from "./resources/systembolaget.json";
+
+export async function main(event, context) {
+  try {
+    const data = await SystembolagetData.items.filter(item => item.group === "Mousserande vin");
+    if (data) {
+      // Return the retrieved item
+      return success(data);
+    } else {
+      return failure({ status: false, error: "Item not found." });
+    }
+  } catch (e) {
+    return failure({ status: false });
+  }
+}
