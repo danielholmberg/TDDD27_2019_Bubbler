250e2ee7b6af02a16378f93854a2b2bd4aa379cf
Daniel Holmberg <danielholmberg.dev@gmail.com>
2019-05-03 10:19:53 +0200

    Added the new WineList component and migrated from react-bootstrap to Semantic-UI-react.

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
-import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
 import { API } from "aws-amplify";
+import { Header, Icon, Button } from "semantic-ui-react";
 
 import "./Home.css";
+import WineList from "../../components/WineList/WineList";
 
 export default class Home extends Component {
   constructor(props) {
@@ -34,37 +34,8 @@ export default class Home extends Component {
     return API.get("wines", "/wines");
   }  
 
-  /**
-   * This method always renders a Create a new wine button as the first item in the list 
-   * (even if the list is empty). We do this by concatenating an array with an 
-   * empty object with our wines array. We render the first line of each wine as 
-   * the ListGroupItem header by doing wine.content.trim().split('\n')[0].
-
-And the LinkContainer component directs our app to each of the items.
-   */
-  renderWinesList(wines) {
-    return [{}].concat(wines).map(
-      (wine, i) =>
-        i !== 0
-          ? <LinkContainer
-              key={wine.wineId}
-              to={`/wines/${wine.wineId}`}
-            >
-              <ListGroupItem header={wine.content.trim().split("\n")[0]}>
-                {"Created: " + new Date(wine.createdAt).toLocaleString()}
-              </ListGroupItem>
-            </LinkContainer>
-          : <LinkContainer
-              key="new"
-              to="/wines/new"
-            >
-              <ListGroupItem className="btn-add">
-                <h4>
-                  <b>{"\uFF0B"}</b> Add more bubbles
-                </h4>
-              </ListGroupItem>
-            </LinkContainer>
-    );
+  renderWineList(wines) {
+    return <WineList items={wines} />
   }  
 
   renderLander() {
@@ -79,10 +50,13 @@ And the LinkContainer component directs our app to each of the items.
   renderWines() {
     return (
       <div className="wines">
-        <PageHeader>Your Bubbles</PageHeader>
-        <ListGroup>
-          {!this.state.isLoading && this.renderWinesList(this.state.wines)}
-        </ListGroup>
+        <Header as='h1'><center>Shared Bubbles</center></Header>
+        <center>
+          <Button primary href="/wines/new" style={{ margin: 16, width: '50%', backgroundColor: 'green' }}>
+            <Icon name='add'/> Add more bubbles
+          </Button>
+        </center>
+        {!this.state.isLoading && this.renderWineList(this.state.wines)}
       </div>
     );
   }
