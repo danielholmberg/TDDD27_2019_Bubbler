b043f302d48973562533521b7048844a371de198
Daniel Holmberg <danielholmberg.dev@gmail.com>
2019-05-05 17:18:20 +0200

    Updated AWS Serverless values again.

--- src/config.js
+++ src/config.js
@@ -3,16 +3,16 @@ export default {
   ACCEPTED_FILE_FORMATS: ["image/png", "image/jpeg", "image/jpg"],
   s3: {
     REGION: "us-east-2",
-    BUCKET: "bubbler-api-prod-imagesbucket-zwyush72ktle"
+    BUCKET: "bubbler-api-prod-imagesbucket-17g8a4s4vk8t7"
   },
   apiGateway: {
     REGION: "us-east-2",
-    URL: "https://n52rvdx36i.execute-api.us-east-2.amazonaws.com/prod"
+    URL: "https://5l09l05h75.execute-api.us-east-2.amazonaws.com/prod"
   },
   cognito: {
     REGION: "us-east-2",
-    USER_POOL_ID: "us-east-2_XvFYyc4fb",
-    APP_CLIENT_ID: "6b6k6b8kuh65sp47krh4o2c8jr",
-    IDENTITY_POOL_ID: "us-east-2:dc57d1c2-166b-4d5d-b92a-bdc3dc370dd4"
+    USER_POOL_ID: "us-east-2_qZhcskiac",
+    APP_CLIENT_ID: "4nb7ec55qiq8hs6mocvniq594q",
+    IDENTITY_POOL_ID: "us-east-2:9323f935-943d-4a39-b756-a43bef451ce6"
   }
 };
