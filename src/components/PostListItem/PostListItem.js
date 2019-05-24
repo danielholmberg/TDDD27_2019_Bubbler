b2ac7dea381c49b0e61e63313afd47e53f3b8ad8
Daniel Holmberg <danielholmberg.dev@gmail.com>
2019-05-24 16:51:18 +0200

    Initial re-design of Post-items, and implementation of Details modal view.

--- src/components/PostListItem/PostListItem.js
+++ src/components/PostListItem/PostListItem.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Storage } from 'aws-amplify';
 import { Link, withRouter } from "react-router-dom";
-import { Card, Rating, Image } from 'semantic-ui-react';
+import { Card, Rating, Image, Modal, Icon, Header, Button, Segment, Item, Divider, Label } from 'semantic-ui-react';
 import { connect } from "react-redux";
 
 import "./PostListItem.css";
@@ -29,21 +29,15 @@ class PostListItem extends Component {
     }
   }
 
-  render() {
+  renderItem() {
     const post = this.props.item;
-    const user = this.props.user;
-    const { userId, postId, image, label, comment, rating, addedAt, updatedAt } = post;
+    const { postId, image, label, comment, rating, addedAt, updatedAt } = post;
+
     return (
-      <Card raised key={postId} as={Link} to={userId === user.id ? `/posts/${postId}` : this.props.location.pathname}>
-        {image &&
-          <center>
-            <Image 
-              rounded 
-              style={{ margin: 10, maxWidth: 200, maxHeight: 200 }}
-              src={this.state.imageURL} 
-              alt={label}
-            />
-          </center>}
+      <Card style={{ overflowWrap: 'break-word' }} raised key={postId} >
+        <Header style={{position: 'relative', margin: 0, padding: 10, backgroundColor: '#57B4D5', color: 'white'}} as='h4'>
+          <Image bordered circular src='https://react.semantic-ui.com/images/avatar/large/patrick.png' /> {this.props.user.attributes.email.split('@')[0]}
+        </Header>
         <Card.Content>
           <Card.Header>{label}</Card.Header>
           <Card.Meta>
@@ -53,10 +47,80 @@ class PostListItem extends Component {
           {comment && <Card.Description>{comment}</Card.Description>}
         </Card.Content>
         <Card.Content extra>
+          {image &&
+            <Image 
+              rounded 
+              floated='left'
+              style={{ margin: 0, marginRight: 10, maxWidth: 100, maxHeight: 100 }}
+              src={this.state.imageURL} 
+              alt={label}
+            />}
           <small>Added: {new Date(addedAt).toLocaleString()}</small>
           <small>{updatedAt && `Updated: ${new Date(updatedAt).toLocaleString()}`}</small>
         </Card.Content>
       </Card>
+    )
+  }
+
+  render() {
+    const post = this.props.item;
+    const user = this.props.user;
+    const { userId, postId, image, label, comment, rating, addedAt, updatedAt } = post;
+
+    const CardItem = this.renderItem();
+
+    return (
+      <Modal 
+        closeIcon={{ style: { top: '1.0535rem', right: '1rem' }, color: 'black', name: 'close' }} // Workaround for button not showing on larger screen sizes.
+        size='small' trigger={CardItem} style={{position: 'relative'}} // postition: 'relative' to get the modal centered and wrapped.
+        >
+
+        <Header content='Review details' />
+        <Modal.Content>
+          <Modal.Description style={{ margin: 'auto', overflowWrap: 'break-word' }}>
+            <Segment>
+              <Item>
+                <Item.Content>
+                  
+                  <Header>{label}</Header>
+                  <Item.Meta>
+                    <Rating 
+                      icon='star' 
+                      style={{ paddingRight: 10, paddingTop: 0 }} 
+                      rating={rating} 
+                      maxRating={5} 
+                      disabled />{rating ? rating : 0}
+                  </Item.Meta>
+                  <Item.Description>
+                    {comment}
+                  </Item.Description>
+                  <Divider></Divider>
+                  <Item.Extra>
+                    <small>Added: {new Date(addedAt).toLocaleString()}</small>
+                    <small>{updatedAt && `Updated: ${new Date(updatedAt).toLocaleString()}`}</small>
+                  </Item.Extra>
+
+                </Item.Content>
+              </Item>
+            </Segment>
+          </Modal.Description>
+          {image &&
+            <Image 
+              rounded 
+              style={{ margin: 'auto', paddingTop: 10, maxHeight: 400, maxWidth: 400 }}
+              src={this.state.imageURL} 
+              alt={label}
+            />}
+        </Modal.Content>
+
+        {user.id === userId ?
+          <Modal.Actions>
+            <Button as={Link} to={`/posts/${postId}`} primary>
+              <Icon name='edit' /> Edit
+            </Button>
+          </Modal.Actions> : null}
+
+      </Modal>
     )
   }
 }
